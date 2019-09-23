328fa737e10f3feac03291a0b00fbf366570c39c
CarolineQuQLJ <1085348932@qq.com>
2019-09-23 20:24:27 +0800

    update the index ftl file

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -5,4 +5,9 @@ INSERT INTO `user` (id, username, password, role) VALUES ('4', 'user2', '1234', 
 INSERT INTO `user` (id, username, password, role) VALUES ('5', 'user3', '1234', 'user');
 INSERT INTO `user` (id, username, password, role) VALUES ('6', 'user4', '1234', 'user');
 
+INSERT INTO `blog` (title,content) VALUES ('第1个博客', '博客内容1');
+INSERT INTO `blog` (title,content) VALUES ('第2个博客', '博客内容2');
+INSERT INTO `blog` (title,content) VALUES ('第3个博客', '博客内容3');
+INSERT INTO `blog` (title,content) VALUES ('第4个博客', '博客内容4');
+
 COMMIT;
